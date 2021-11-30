a1ce6041559a6c501eb5a112ecebbd93318109f0
Katelyn <katelynjewel@gmail.com>
2021-11-30 11:48:13 -0800

    commit tree 32257b0071d64c56ec331eca676612229d95e246

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def render_invalid(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def not_found
+    render json: { error: error.message}, status: :not_found
+  end
+
 end
